561d1666c53a0b4908205fa06dff064d6beeb204
darkropo <rodolfo.noguera@gmail.com>
2019-12-28 10:03:47 -0300

    se modifican varias apis para correr infinitamente

--- app/controllers/populador_bit_bay_controller.rb
+++ app/controllers/populador_bit_bay_controller.rb
@@ -20,35 +20,38 @@ class PopuladorBitBayController < ActionController::API
       end
 
       apix = HttpCall.new
-      respuesta = apix.call('https://bitbay.net/API/Public/BTCUSD/trades.json?since='+since)
-      #info log
-      logger.debug "bitBay_call:" + 'https://bitbay.net/API/Public/BTCUSD/trades.json?since='+since.to_s 
-      logger.debug "bitBay_result_code: " + respuesta.code.to_s
-     
-      if respuesta.code.to_s === "200"
-        res = JSON.parse(respuesta.body)
-        @result = collection.insert_many(res)
-        #info log
-        logger.debug "bitBay_result_inserted_trades: " + @result.inserted_count.to_s
-        @response = '{
-          "response":[{
-            "status": "ok"
-            "code": "200",
-          }]
-        }'
-        
-      else
-        #info log
-        logger.debug "bitBay_result_code: " + respuesta.code.to_s
-        logger.debug "bitBay_result_error: " + respuesta.body
-        @response = '{
-          "response":[{
-            "status": "error"
-            "code": "'+respuesta.code.to_s+'",
-          }]
-        }'
-        
-      end
+      while true
+        sleep 10
+            respuesta = apix.call('https://bitbay.net/API/Public/BTCUSD/trades.json?since='+since)
+            #info log
+            logger.debug "bitBay_call:" + 'https://bitbay.net/API/Public/BTCUSD/trades.json?since='+since.to_s 
+            logger.debug "bitBay_result_code: " + respuesta.code.to_s
+          
+            if respuesta.code.to_s === "200"
+              res = JSON.parse(respuesta.body)
+              @result = collection.insert_many(res)
+              #info log
+              logger.debug "bitBay_result_inserted_trades: " + @result.inserted_count.to_s
+              @response = '{
+                "response":[{
+                  "status": "ok"
+                  "code": "200",
+                }]
+              }'
+              
+            else
+              #info log
+              logger.debug "bitBay_result_code: " + respuesta.code.to_s
+              logger.debug "bitBay_result_error: " + respuesta.body
+              @response = '{
+                "response":[{
+                  "status": "error"
+                  "code": "'+respuesta.code.to_s+'",
+                }]
+              }'
+              
+            end
+          end
       json_response(@response)
     end
 
---
+++ app/controllers/populador_coinbase_controller.rb
@@ -0,0 +1,48 @@
+####
+###
+## For Documentation
+# https://currentmillis.com/ 
+# https://docs.gemini.com/rest-api/#trade-history
+require 'em/pure_ruby'
+require 'coinbase/exchange'
+require 'eventmachine'
+
+class PopuladorCoinbaseController < ActionController::API
+    include Response
+    include ExceptionHandler
+    
+    def populateCoinbase
+        #Estas valores estan hardcodeados aca por que el api solo tiene permiso de lectura
+        #si se llegara a cambiar los permisos se deberia pasar estos valores por parametros por seguridad
+
+        api_key = "5688bf3d3bac1521c66ef50e245ede2c"
+        api_secret = "Vac1vhlTRhSznw1IpxQy/lVOtjJlJbzmb2EN/poG5NgjIKku1GcJWFw4oOJhdkgarRPd+/A1tzC3b/whR6wIUQ=="
+        api_pass = "krypgen8945"
+
+        conn = DatabaseConection.new
+        client = conn.connect      
+        collection = client[:gen_coinbase]
+        find_tid = collection.find().sort({timestamp:-1}).first
+        if find_tid.present?
+            since = find_tid[:timestamp]     
+        else
+            ##este timestamp es en milisegundos
+            since = params[:since].to_i
+        end
+
+        rest_api = Coinbase::Exchange::AsyncClient.new(api_key, api_secret, api_pass)
+        EM.run {
+            EM.add_periodic_timer(10) {
+              rest_api.last_trade(product_id: "BTC-USD") do |resp|
+                res = JSON.parse(resp)
+                @result = collection.insert_many(res)
+              end
+            }
+          }
+        #info log
+        #logger.debug "coinbase_call:" + 'https://api.gemini.com/v1/trades/btcusd?limit_trades=500&timestamp='+since.to_s 
+        #logger.debug "gemini_result_code: " + respuesta.code.to_s
+
+        
+    end
+end
